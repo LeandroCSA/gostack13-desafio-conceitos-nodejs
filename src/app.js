40aa7d6525d9cbbb8d73c544a2a877a3499792c4
LeandroCSA <leandro_csa@yahoo.com.br>
2020-08-18 15:10:03 -0300

    Desafio node - GoStack 13

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { uuid, isUuid } = require("uuidv4");
 
 // const { uuid } = require("uuidv4");
 
@@ -10,24 +11,103 @@ app.use(cors());
 
 const repositories = [];
 
+function validateRepositoryId(req, res, next){
+  const {
+    id
+  } = req.params
+
+  if(!isUuid(id))
+      return res.status(400).json({error: 'Invalid repository id'})
+  
+  return next()
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {
+    title,
+    url,
+    techs
+  } = request.body;
+  
+  const likes = 0;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateRepositoryId, (request, response) => {
+  const {
+    id
+  } = request.params;
+
+  const {
+    title,
+    url,
+    techs
+  } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => {
+    return repository.id === id
+  });
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not fund." });
+  }
+
+  repositories[repositoryIndex].title = title;
+  repositories[repositoryIndex].url = url;
+  repositories[repositoryIndex].techs = techs;
+
+  return response.status(200).json(repositories[repositoryIndex]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateRepositoryId, (request, response) => {
+  const {
+    id
+  } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => {
+    return repository.id === id
+  });
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: "Repository not fund." });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).json({});  
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateRepositoryId, (request, response) => {
+  const {
+    id
+  } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => {
+    return repository.id === id
+  });
+
+  if(repositoryIndex < 0) { 
+    return response.status(400).json({error: 'Repository not found!'})
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  return response.status(200).json({likes: repositories[repositoryIndex].likes});
 });
 
 module.exports = app;
